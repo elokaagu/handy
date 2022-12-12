08a9659f8ff26986d58f78b1b4eae64d8063be5b
elokaagu <eloka@satellitelabs.xyz>
2022-12-12 16:19:54 +0000

    hand recognition

--- src/App.js
+++ src/App.js
@@ -1,18 +1,27 @@
 import logo from "./logo.svg";
 import "./App.css";
-import React, { useRef } from "react";
+import React, { useRef, useState } from "react";
 import * as tf from "@tensorflow/tfjs";
 import * as handpose from "@tensorflow-models/handpose";
 import Webcam from "react-webcam";
 import { drawHand } from "./utilities";
 
+// Import images
+import * as fp from "fingerpose";
+import rock from "../src/images/rock.png";
+import paper from "../src/images/paper.png";
+// import scissors from "../src/images/scissors.png";
+
 function App() {
   const webcamRef = useRef(null);
   const canvasRef = useRef(null);
 
+  const [emoji, setEmoji] = useState(null);
+  const images = { rock: rock, paper: paper };
+
   const runHandpose = async () => {
     const net = await handpose.load();
-    console.log("Handpose model loaded.");
+    // console.log("Handpose model loaded.");
     // Loop and detect hands
     setInterval(() => {
       detect(net);
@@ -41,8 +50,28 @@ function App() {
 
       // Make Detections
       const hand = await net.estimateHands(video);
-      console.log(hand);
+      // console.log(hand);
 
+      if (hand.length > 0) {
+        const GE = new fp.GestureEstimator([
+          fp.Gestures.VictoryGesture,
+          fp.Gestures.ThumbsUpGesture,
+        ]);
+
+        const gesture = await GE.estimate(hand[0].landmarks, 8);
+        console.log(gesture);
+
+        if (gesture.gestures !== undefined && gesture.gestures.length > 0) {
+          const confidence = gesture.gestures.map(
+            (prediction) => prediction.confidence
+          );
+          const maxConfidence = confidence.indexOf(
+            Math.max.apply(null, confidence)
+          );
+          // setEmoji(gesture.gestures[maxConfidence].name);
+          // console.log(emoji);
+        }
+      }
       // Draw mesh
       const ctx = canvasRef.current.getContext("2d");
       drawHand(hand, ctx);
@@ -83,6 +112,23 @@ function App() {
             height: 480,
           }}
         />
+        {emoji !== null
+          ? ((
+              <img
+                src={images[emoji]}
+                style={{
+                  position: "absolute",
+                  marginLeft: "auto",
+                  marginRight: "auto",
+                  left: 400,
+                  right: 0,
+                  bottom: 500,
+                  textAlign: "center",
+                  height: 100,
+                }}
+              />
+            ): "")
+          : ""}
       </header>
     </div>
   );
