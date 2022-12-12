ec253e1ca2c2aadf81804e4c6ba6a68e36a59566
elokaagu <eloka@satellitelabs.xyz>
2022-12-12 14:13:48 +0000

    added stroke to hand

--- src/utilities.js
+++ src/utilities.js
@@ -1,3 +1,11 @@
+const fingerJoints = {
+  thumb: [0, 1, 2, 3, 4],
+  indexFinger: [0, 5, 6, 7, 8],
+  middleFinger: [0, 9, 10, 11, 12],
+  ringFinger: [0, 13, 14, 15, 16],
+  pinky: [0, 17, 18, 19, 20],
+};
+
 // Drawing function
 export const drawHand = (predictions, ctx) => {
   // Check if we have predictions
@@ -6,6 +14,30 @@ export const drawHand = (predictions, ctx) => {
     predictions.forEach((prediction) => {
       // Grab landmarks
       const landmarks = prediction.landmarks;
+
+      // Loop through fingers
+      for (let j = 0; j < Object.keys(fingerJoints).length; j++) {
+        let finger = Object.keys(fingerJoints)[j];
+        // Loop through pairs of joints
+        for (let k = 0; k < fingerJoints[finger].length - 1; k++) {
+          const firstJointIndex = fingerJoints[finger][k];
+          const secondJointIndex = fingerJoints[finger][k + 1];
+
+          // Draw path
+          ctx.beginPath();
+          ctx.moveTo(
+            landmarks[firstJointIndex][0],
+            landmarks[firstJointIndex][1]
+          );
+          ctx.lineTo(
+            landmarks[secondJointIndex][0],
+            landmarks[secondJointIndex][1]
+          );
+          ctx.strokeStyle = "plum";
+          ctx.lineWidth = 4;
+          ctx.stroke();
+        }
+      }
 
       // Loop through landmarks and draw em
       for (let i = 0; i < landmarks.length; i++) {
@@ -17,7 +49,7 @@ export const drawHand = (predictions, ctx) => {
         ctx.beginPath();
         ctx.arc(x, y, 5, 0, 3 * Math.PI);
         // Set line colour
-        ctx.fillStyle = "indigo";
+        ctx.fillStyle = "black";
         ctx.fill();
       }
     });
