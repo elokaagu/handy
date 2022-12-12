85939c5d34dffaf7ecf65722965cd7ba39b7b25a
elokaagu <eloka@satellitelabs.xyz>
2022-12-12 13:57:00 +0000

    first commit

---
+++ src/utilities.js
@@ -0,0 +1,25 @@
+// Drawing function
+export const drawHand = (predictions, ctx) => {
+  // Check if we have predictions
+  if (predictions.length > 0) {
+    // Loop through each prediction
+    predictions.forEach((prediction) => {
+      // Grab landmarks
+      const landmarks = prediction.landmarks;
+
+      // Loop through landmarks and draw em
+      for (let i = 0; i < landmarks.length; i++) {
+        // Get x point
+        const x = landmarks[i][0];
+        // Get y point
+        const y = landmarks[i][1];
+        // Start drawing
+        ctx.beginPath();
+        ctx.arc(x, y, 5, 0, 3 * Math.PI);
+        // Set line colour
+        ctx.fillStyle = "indigo";
+        ctx.fill();
+      }
+    });
+  }
+};
